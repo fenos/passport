1c9918ad6bd4e1d712de87de483a7c8d5a4f8262
Fabrizio <fabri_feno@yahoo.it>
2017-06-11 12:34:16 +0100

    support UUID for client id

--- src/Client.php
+++ src/Client.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+
+    /**
+     * @var bool
+     */
+    public $incrementing = false;
+
     /**
      * The database table used by the model.
      *
